90425c3812837343678fbaa8f56434fb9855c40d
Danielo814 <ojedadaniel92@gmail.com>
2019-10-03 13:52:04 -0700

    make convert a wrapper function, add two new methods for checking what we are converting

--- converter/script.js
+++ converter/script.js
@@ -1,23 +1,33 @@
 function convert() {
+    function toFahrenheit(input) {
+        var celcius = ((parseInt(input, 10) - 32) *  (5 / 9)).toFixed(2) + "&#8451";
+        return celcius;
+    }
+
+    function toCelcius(input) {
+        var farenheit = ((parseInt(input, 10) * 1.8) + 32).toFixed(2) + "&#8457";
+        return farenheit;
+    }
+
+    function isFahrenheit(input) {
+        if (input[input.length - 1] === "f" ||  input[input.length - 1] === "F") {
+            return true;
+        }
+    }
+
+    function isCelcius(input) {
+        if (input[input.length - 1] === "C" ||  input[input.length - 1] === "c" || num.length > 0) {
+            return true;
+        }
+    }
     var num = document.getElementById("input").value;
-    if (num[num.length - 1] === "f" ||  num[num.length - 1] === "F") {
-        temperature = toFarenheit();
-    } else if ((num[num.length - 1] === "C" ||  num[num.length - 1] === "c" || num.length > 0)) {
-        temperature = toCelcius();
+    if (isFahrenheit(num)) {
+        temperature = toFahrenheit(num);
+    } else if (isCelcius(input)) {
+        temperature = toCelcius(num);
     } else {
         temperature = "";
     }
     document.getElementById("output").innerHTML = temperature;
 }
 
-function toFarenheit() {
-    var farenheit = document.getElementById("input").value;
-    var celcius = ((parseInt(farenheit, 10) - 32) *  (5 / 9)).toFixed(2) + "&#8451";
-    return celcius;
-}
-
-function toCelcius() {
-    var celcius = document.getElementById("input").value;
-    var farenheit = ((parseInt(celcius, 10) * 1.8) + 32).toFixed(2) + "&#8457";
-    return farenheit;
-}
