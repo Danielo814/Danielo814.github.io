a2e512cfb4db3e44a8f73e76dd131fa20906dc81
Danielo814 <ojedadaniel92@gmail.com>
2019-10-03 14:23:50 -0700

    move functions outside of convert

--- converter/script.js
+++ converter/script.js
@@ -1,33 +1,34 @@
+function getInput() {
+    return document.getElementById("input").value;
+}
+
+function toFahrenheit(input) {
+    var celcius = ((parseInt(input, 10) - 32) *  (5 / 9)).toFixed(2) + "&#8451";
+    return celcius;
+}
+
+function toCelcius(input) {
+    var farenheit = ((parseInt(input, 10) * 1.8) + 32).toFixed(2) + "&#8457";
+    return farenheit;
+}
+
+function isFahrenheit(input) {
+    return input[input.length - 1] === "f" ||  input[input.length - 1] === "F";
+
+}
+
+function isCelcius(input) {
+    return input[input.length - 1] === "C" ||  input[input.length - 1] === "c";
+}
+
 function convert() {
-    function toFahrenheit(input) {
-        var celcius = ((parseInt(input, 10) - 32) *  (5 / 9)).toFixed(2) + "&#8451";
-        return celcius;
-    }
-
-    function toCelcius(input) {
-        var farenheit = ((parseInt(input, 10) * 1.8) + 32).toFixed(2) + "&#8457";
-        return farenheit;
-    }
-
-    function isFahrenheit(input) {
-        if (input[input.length - 1] === "f" ||  input[input.length - 1] === "F") {
-            return true;
-        }
-    }
-
-    function isCelcius(input) {
-        if (input[input.length - 1] === "C" ||  input[input.length - 1] === "c" || num.length > 0) {
-            return true;
-        }
-    }
-    var num = document.getElementById("input").value;
-    if (isFahrenheit(num)) {
-        temperature = toFahrenheit(num);
-    } else if (isCelcius(input)) {
-        temperature = toCelcius(num);
+    var input = getInput();
+    if (isFahrenheit(input)) {
+        temperature = toFahrenheit(input);
+    } else if (isCelcius(input) || input.length > 0) {
+        temperature = toCelcius(input);
     } else {
         temperature = "";
     }
     document.getElementById("output").innerHTML = temperature;
 }
-
