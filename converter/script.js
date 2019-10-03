2b89de56e09a296e1f3cb00ae60d9aa02b51e481
Danielo814 <ojedadaniel92@gmail.com>
2019-10-03 11:52:31 -0700

    split up code, change variable names

--- converter/script.js
+++ converter/script.js
@@ -1,12 +1,23 @@
-function getValue() {
-    var num = document.getElementById("celc").value;
+function convert() {
+    var num = document.getElementById("input").value;
     if (num[num.length - 1] === "f" ||  num[num.length - 1] === "F") {
-        celcius = ((parseInt(num, 10) - 32) *  (5 / 9)).toFixed(2) + "&#8451";
+        temperature = toFarenheit();
     } else if ((num[num.length - 1] === "C" ||  num[num.length - 1] === "c" || num.length > 0)) {
-        celcius = ((parseInt(num, 10) * 1.8) + 32).toFixed(2) + "&#8457";
-        // document.getElementById("box").innerHTML = celcius + "&#8457";
+        temperature = toCelcius();
     } else {
-        celcius = "";
+        temperature = "";
     }
-    document.getElementById("box").innerHTML = celcius;
+    document.getElementById("output").innerHTML = temperature;
+}
+
+function toFarenheit() {
+    var farenheit = document.getElementById("input").value;
+    var celcius = ((parseInt(farenheit, 10) - 32) *  (5 / 9)).toFixed(2) + "&#8451";
+    return celcius;
+}
+
+function toCelcius() {
+    var celcius = document.getElementById("input").value;
+    var farenheit = ((parseInt(celcius, 10) * 1.8) + 32).toFixed(2) + "&#8457";
+    return farenheit;
 }
