de8c9c7aea05ffc7c88f99eb10c788c9c7790c5e
Danielo814 <ojedadaniel92@gmail.com>
2019-10-28 20:21:43 -0700

    remove date objects

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,22 +1,34 @@
 #!/usr/bin/env node
-
- function countdown() {
-    var endtime = new Date();
-    var timerValue = document.getElementById("input").value;
-    endtime.setMinutes(endtime.getMinutes() + Number(timerValue));
-
-    var myVar = setInterval(function() {
-    var starttime = new Date();
-
-    let result = Math.round((endtime.getTime() - starttime.getTime()) / 1000);
-    console.log(result);
-
-    if (result <= 0) {
-        clearInterval(myVar);
-    } else {
-        document.getElementById("seconds").innerHTML = result + " s";
+function countDown() {
+    var timerValue = Number(document.getElementById("input").value);
+    var myInterval = setInterval(update, 1000);
+    function update() {
+        timerValue--;
+        if (timerValue < 0) {
+            clearInterval(myInterval)
+        } else{
+            document.getElementById("seconds").innerHTML = timerValue + " s";
+        }
     }
-
-    }, 1000);
-   
 }
+
+//  function countdown() {
+//     var endtime = new Date();
+//     var timerValue = document.getElementById("input").value;
+//     endtime.setMinutes(endtime.getMinutes() + Number(timerValue));
+
+//     var myVar = setInterval(function() {
+//     var starttime = new Date();
+
+//     let result = Math.round((endtime.getTime() - starttime.getTime()) / 1000);
+//     console.log(result);
+
+//     if (result <= 0) {
+//         clearInterval(myVar);
+//     } else {
+//         document.getElementById("seconds").innerHTML = result + " s";
+//     }
+
+//     }, 1000);
+   
+// }
