787628b254797909a4873bb059ec8737906a7497
Danielo814 <ojedadaniel92@gmail.com>
2020-02-16 14:35:51 -0800

    add build

---
+++ site_makeover/build/precache-manifest.04d7a9424fe7b848f7a8b4488073cbd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eca17898f344f96517979b90db95ca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e0cf4f6724a3e0423fc",
+    "url": "/static/css/main.eefd6b8d.chunk.css"
+  },
+  {
+    "revision": "092d1d9306ea2ae0fc78",
+    "url": "/static/js/2.2d289b44.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2d289b44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e0cf4f6724a3e0423fc",
+    "url": "/static/js/main.4f67847b.chunk.js"
+  },
+  {
+    "revision": "0d2675136d51f4c80708",
+    "url": "/static/js/runtime-main.7fe859ef.js"
+  }
+]);
