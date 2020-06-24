697d8bf5b7b4255abd1088994333b041be2eeac6
Daniel Ojeda <ojedadaniel92@gmail.com>
2020-06-24 12:25:18 -0700

    clean

--- site_makeover/src/components/blog/client/components/App/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = (props) => {
-  return <p>Hello App</p>;
-};
-
-export default App;
--- site_makeover/src/components/homepage/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Logo = () => {
-  return <div className="logo-box">DO</div>;
-};
-
-export default Logo;
